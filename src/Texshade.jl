d8da098a059807d7a87ccbaa93ac006f32c41c98
Ahmed Fasih <wuzzyview@gmail.com>
2017-06-30 14:19:50 -0400

    Faster frequency-domain filtering

--- src/Texshade.jl
+++ src/Texshade.jl
@@ -3,23 +3,65 @@ module Texshade
 export texshade;
 
 mkf(n::Integer) = ((0 : (n - 1)) - (n >> 1)) / n
+mkf2(n::Integer) = ifftshift(((0 : (n - 1)) - (n >> 1)) / n)
 
 function texshade(x; alpha::Real=0.5)
   n = map(x -> nextprod([2,3,5,7], x), size(x))
   if n == size(x)
-    return core(x; alpha)
+    return core(x, alpha)
   end
 
   x2 = zeros(n)
   x2[1:size(x,1), 1:size(x,2)] = x
-  y2 = core(x2; alpha=alpha)
-  return y2[1:size(x,1), 1:size(x,2)]
+  y2 = core(x2, alpha)
+  y2[1:size(x,1), 1:size(x,2)]
 end
 
 
-function core(x; alpha::Real=0.5)
+function core(x, alpha)
   mapped = map(t -> (t[1]*t[1] + t[2]*t[2]) .^ (alpha * 0.5), Base.product(mkf(size(x, 1)), mkf(size(x, 2))))
-  return real(ifft(ifftshift(mapped .* fftshift(fft(x)))))
+  real(ifft(ifftshift(mapped .* fftshift(fft(x)))))
+end
+
+function core2(x, alpha)
+  xf = fft(x)
+  for (i, (f1, f2)) in Base.enumerate(Base.product(mkf2(size(x, 1)), mkf2(size(x, 2))))
+    xf[i] *= (f1 * f1 + f2 * f2) .^ (alpha * 0.5)
+  end
+  real(ifft(xf))
+end
+
+function innercore3!(x, alpha)
+  assert(all(x -> x % 2 == 0, size(x)))
+  (n1, n2) = size(x)
+  d1 = 1.0 / n1
+  alpha = 0.5 * alpha
+
+  for i2 = 1 : n2
+    local f2sq = if (i2 <= (n2 ÷ 2))
+      (i2 - 1) / n2 else
+      (n2 - i2 + 1) / -n2 end # superfluous minus sign since it's going to be ^2
+    f2sq *= f2sq
+
+    local f1 = 0.0
+    for i1 in 1 : (n1 ÷ 2)
+      x[i1, i2] *= (f1 * f1 + f2sq) .^ alpha
+      f1 += d1
+    end
+
+    f1 = -0.5
+    for i1 in (n1 ÷ 2 + 1) : n1
+      x[i1, i2] *= (f1 * f1 + f2sq) .^ alpha
+      f1 += d1
+    end
+  end
+end
+
+function core3(x, alpha)
+  xf = fft(x)
+  innercore3!(xf, alpha)
+  ifft!(xf)
+  real(xf)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,36 @@
 using Texshade
+reload("Texshade")
 using Base.Test
+
+mkf(n) = ((0 : (n - 1)) - (n >> 1)) / n
+alpha = 0.5
 
 x = randn(17,17)
 XF = fftshift(fft(x))
 
-mkf(n) = ((0 : (n - 1)) - (n >> 1)) / n
-
-alpha = 0.5
-
 y = real(ifft(ifftshift(map(t->(t[1]*t[1] + t[2]*t[2]) .^ (alpha / 2), Base.product(mkf(size(XF,1)), mkf(size(XF,2)))) .* XF)))
+
+@test all(x->isapprox(x,0), Texshade.core2(x, alpha) - y)
+@test all(x->isapprox(x,0), Texshade.core(x, alpha) - y)
 
 x2 = zeros(18,18)
 x2[1:17,1:17] = x
+y2 = real(ifft(ifftshift(map(t->(t[1]*t[1] + t[2]*t[2]) .^ (alpha / 2), Base.product(mkf(18), mkf(18))) .* fftshift(fft(x2)))))
 
-y2 = real(ifft(ifftshift(map(t->(t[1]*t[1] + t[2]*t[2]) .^ (alpha / 2), Base.product(mkf(32), mkf(32))) .* fftshift(fft(x2)))))
+@test all(x->isapprox(x,0), Texshade.texshade(x; alpha=alpha) - y2[1:17, 1:17])
 
-@test 1 == 2
+@test all(x->isapprox(x,0), Texshade.core2(x2, alpha) - y2)
 
+@test all(x->isapprox(x,0; atol=2 * eps()), Texshade.core3(x2, alpha) - y2)
 
-texshade(x)-y2[1:17,1:17]
+x = randn(3000,3000)
+
+using BenchmarkTools
+@benchmark Texshade.core(x, alpha)
+@benchmark Texshade.core2(x, alpha)
+@benchmark Texshade.core3(x, alpha)
+
+@time Texshade.core(x, alpha);
+@time Texshade.core2(x, alpha);
+@time Texshade.core3(x, alpha);
+@time Texshade.innercore3!(x, alpha);
